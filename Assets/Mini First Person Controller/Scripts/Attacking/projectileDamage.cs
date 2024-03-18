8eb7ad1c2c1a06b272006afb809071aae428b049
DSEArielle <admiralavaoni@gmail.com>
2024-03-18 11:54:50 -0600

    Updated projectileDamage

--- Assets/Mini First Person Controller/Scripts/Attacking/projectileDamage.cs
+++ Assets/Mini First Person Controller/Scripts/Attacking/projectileDamage.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class projectileDamage : MonoBehaviour
 {
-    [SerializeField] private int damage = 50;
+    [SerializeField] private int damage = 10;
 
     //detects collision with enemy
     private void OnCollisionEnter(Collision collision)
@@ -14,7 +14,20 @@ public class projectileDamage : MonoBehaviour
             enemyHealth enemy = collision.gameObject.GetComponent<enemyHealth>();
             if (enemy != null)
             {
+                // Temporarily set the enemy's Rigidbody to kinematic to prevent pushing
+                Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+                if (enemyRigidbody != null)
+                {
+                    enemyRigidbody.isKinematic = true;
+                }
+
                 enemy.TakeDamage(damage);
+
+                // Reset the enemy's Rigidbody to non-kinematic after taking damage
+                if (enemyRigidbody != null)
+                {
+                    enemyRigidbody.isKinematic = false;
+                }
             }
             Destroy(gameObject); // Destroy the projectile after it hits the enemy
         }
