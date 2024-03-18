fd9f011678ea043ec9328a68d5bda65cfb9880ae
DSEArielle <admiralavaoni@gmail.com>
2024-03-17 23:16:56 -0600

    Player - Death
    Player is killed by the projectile when it hits

--- Assets/Props/Projectiles/Scripts/damage.cs
+++ Assets/Props/Projectiles/Scripts/damage.cs
@@ -2,8 +2,15 @@
 
 namespace Props.Projectiles.Scripts
 {
-    public class damage : MonoBehaviour
+    public class Damage : MonoBehaviour
     {
-        
+        // When the projectile collides with the player, destroy the player object
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                Destroy(collision.gameObject);
+            }
+        }
     }
 }
